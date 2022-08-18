546aaff3d06c5038e65d10120e824942ad6ec660
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-17 20:47:30 -0400

    moving function data to apiRoutes file, cleaned code and added comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,30 +1,21 @@
-// const fs = require("fs");
+// Required constants for path and router
 const path = require('path');
 const router = require('express').Router();
 
+// returns index.html 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// returns notes.html
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+// default returns to index
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Add function for create note
-function getNote() {};
-function createNote() {};
-// createNote
-// get note with GET, save note with POST
-
-// Add function for delete note
-function deleteNote() {};
-//DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
-//In order to delete a note, you'll need to read all notes from the db.json file, remove the note
-//with the given id property, and then rewrite the notes to the db.json file.
-// delete note with DELETE
-
+// exports router
 module.exports = router;
