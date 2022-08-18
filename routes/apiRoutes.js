bdf7f807bb13aba9799ec9c7eab982029a37e684
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-08-17 20:19:20 -0400

    added apiRoutes.js file, need to update to pull saved note info from db file.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+// const fs = require("fs");
+const path = require('path');
+const router = require('express').Router();
+
+// will need to adjust to pull note info from db file
+router.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Add function for create note
+function getNote() {};
+function createNote() {};
+// createNote
+// get note with GET, save note with POST
+
+// Add function for delete note
+function deleteNote() {};
+//DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
+//In order to delete a note, you'll need to read all notes from the db.json file, remove the note
+//with the given id property, and then rewrite the notes to the db.json file.
+// delete note with DELETE
+
+module.exports = router;
